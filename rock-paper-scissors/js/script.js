cfe699d559062a2d0700e75aaa5ceab4aa1a256e
coinfilip <122690495+coinfilip@users.noreply.github.com>
2023-08-22 21:49:00 +0800

    fix: populate getComputerChoice func and create getRandomNum func

--- rock-paper-scissors/js/script.js
+++ rock-paper-scissors/js/script.js
@@ -1,3 +1,21 @@
 function getComputerChoice() {
-    // should return a random response of either 'Rock', 'Paper' or 'Scissors'
+    // return a response of either 'Rock', 'Paper' or 'Scissors' based on result of getRandomNum()
+    let choice = getRandomNum();
+
+    switch(choice) {
+        case 1:
+            return 'Rock';
+            break;
+        case 2:
+            return 'Paper';
+            break;
+        case 3:
+            return 'Scissors';
+            break;
+    }
+}
+
+function getRandomNum() {
+    // returns a random number of either 1, 2 or 3
+    return Math.floor(Math.random() * 3) + 1;
 }
