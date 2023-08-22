c0460d864ff3712685b256a1d10df58ee54152c6
coinfilip <122690495+coinfilip@users.noreply.github.com>
2023-08-22 22:29:06 +0800

    add playRound and getVerdict func to compare player and computer choice

--- rock-paper-scissors/js/script.js
+++ rock-paper-scissors/js/script.js
@@ -1,3 +1,43 @@
+function playRound(playerSelection, computerSelection) {
+    let verdict;
+    let winner;
+    let loser;
+    
+    verdict = getVerdict(playerSelection, computerSelection);
+    
+    if (verdict == 'tie') {
+        return `It's a ${verdict}!`;
+    } else {
+        if (verdict == 'win') {
+            winner = playerSelection;
+            loser = computerSelection;
+        } else {
+            winner = computerSelection;
+            loser = playerSelection;
+        }
+
+        return `You ${verdict}! ${winner} beats ${loser}`;
+    } 
+}
+
+function getVerdict(player, computer) {
+    if (player == computer) {
+        return 'tie';
+    } else if (player == 'Rock' && computer == 'Paper') {
+        return 'lose';
+    } else if (player == 'Rock' && computer == 'Scissors') {
+        return 'win';
+    } else if (player == 'Paper' && computer == 'Scissors') {
+        return 'lose';
+    } else if (player == 'Paper' && computer == 'Rock') {
+        return 'win';
+    } else if (player == 'Scissors' && computer == 'Paper') {
+        return 'win';
+    } else {
+        return 'lose';
+    }
+}
+
 function getComputerChoice() {
     // return a response of either 'Rock', 'Paper' or 'Scissors' based on result of getRandomNum()
     let choice = getRandomNum();
